3ba07270152fbe602fe4919de846a1769c697a35
PopeFrancisOgbonna <francisogbonna24@gmail.com>
2019-10-21 19:40:39 +0100

    Initial commit

---
+++ PDF_Reader/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PDF_Reader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            try
+            {
+                open.Filter = "pdf files(*.pdf)|*.pdf";
+                if (open.ShowDialog() == DialogResult.OK)
+                {
+                    string pdf = open.FileName;
+                    axNitroPDF1.LoadFile(pdf);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
